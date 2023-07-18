fd82e1fcf70395e20be54b7ad6bc70ba1bd396e1
kameiryohei <130110817+kameiryohei@users.noreply.github.com>
2023-07-18 12:01:42 +0900

    0718 am

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-// 例: クリックしたリンクのテキストをコンソールに出力する
-const links = document.querySelectorAll('a');
-links.forEach(link => {
-  link.addEventListener('click', () => {
-    console.log(link.innerText);
+// ページが読み込まれたときに実行する処理
+window.addEventListener('load', function () {
+  // ナビゲーションメニューのアクティブなリンクにクラスを追加する
+  const currentPath = window.location.pathname;
+  const navLinks = document.querySelectorAll('nav a');
+  
+  navLinks.forEach(link => {
+    if (link.getAttribute('href') === currentPath) {
+      link.classList.add('active');
+    }
+  });
+  
+  // スクロール時にヘッダーの背景を変更する
+  const header = document.getElementById('header');
+  
+  window.addEventListener('scroll', function () {
+    if (window.scrollY > 200) {
+      header.classList.add('scrolled');
+    } else {
+      header.classList.remove('scrolled');
+    }
   });
 });
